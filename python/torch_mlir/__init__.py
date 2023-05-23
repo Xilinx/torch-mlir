c373254e090034bc1a106943932e50c350091b86
Matthias Gehre <matthias.gehre@xilinx.com>
2023-05-23 09:41:40 +0200

    do(): Rewrite dataclasses outputs into tuples

--- python/torch_mlir/__init__.py
+++ python/torch_mlir/__init__.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 # Also available under a BSD-style license. See LICENSE.
 
+import dataclasses
 from typing import Optional, Sequence, Union, List, Dict, Tuple, Callable, Iterable
 from enum import Enum
 import importlib.metadata
@@ -479,10 +480,17 @@ def do(model: torch.nn.Module,
     output = model(*model_args, **model_kwargs)
 
     def flatten(S):
+        """
+        Flattens a tree of list/tuples into a flat list.
+        Removes list entries that are None.
+        """
         if len(S) == 0:
             return S
         if isinstance(S[0], list) or isinstance(S[0], tuple):
             return list(flatten(S[0])) + list(flatten(S[1:]))
+        if S[0] is None:
+            return list(flatten(S[1:]))
+
         return list(S[:1]) + list(flatten(S[1:]))
 
     class Wrapper(torch.nn.Module):
@@ -493,12 +501,28 @@ def do(model: torch.nn.Module,
         def forward(self, *args, **kwargs):
             ret = self.model(*args, **kwargs)
             
+            # Torch MLIR does not support return types that are dataclasses
+            # or lists or nested tuples.
+            # It also does not support tuples where some elements are None.
+            # Potential pytorch solution:
+            #   ret, treespec = torch.utils._pytree.tree_flatten(ret)
+            # but unfortunately, pytree doesn't support dataclasses
+            # and it doesn't traverse base classes to see that transformer
+            # outputs derive from OrderedDicts.
+            # TODO: Remember the transformations done here, so we can revert
+            # them outside of the model to restore the original output type.
+            # See approach in make_simple_dynamo_backend.
+
+            if dataclasses.is_dataclass(ret):
+                ret = tuple([ret.__dict__[field.name] for field in dataclasses.fields(ret)])
+
             if isinstance(ret, list) or isinstance(ret, tuple):
                 ret = flatten(ret)
                 if len(ret) == 1:
                     return ret[0]
                 else:
                     return tuple(ret)
+
             return ret
 
     model = Wrapper(model)
