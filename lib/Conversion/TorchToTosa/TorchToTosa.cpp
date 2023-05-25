4ef2fea7ca2d0bb13d8371d0d09532ba60d4ba67
Matthias Gehre <matthias.gehre@xilinx.com>
2023-05-25 15:07:47 +0200

    lib/Conversion/TorchToTosa/TorchToTosa.cpp: Fix slice start/end out of range/none

--- lib/Conversion/TorchToTosa/TorchToTosa.cpp
+++ lib/Conversion/TorchToTosa/TorchToTosa.cpp
@@ -3215,9 +3215,15 @@ LogicalResult ConvertAtenOp<AtenSliceTensorOp>::matchAndRewrite(
   if (start < 0)
     return rewriter.notifyMatchFailure(op, "Currently unsupported: start < 0");
 
+  start = std::min(selfType.getShape()[dim], start);
+
   int64_t end;
-  if (!matchPattern(op.getEnd(), m_TorchConstantInt(&end)))
-    return rewriter.notifyMatchFailure(op, "end must be a Scalar constant");
+  if (!matchPattern(op.getEnd(), m_TorchConstantInt(&end))) {
+    if (isa<ConstantNoneOp>(op.getEnd().getDefiningOp()))
+      end = selfType.getShape()[dim];
+    else
+      return rewriter.notifyMatchFailure(op, "end must be a Scalar constant");
+  }
   // support for end < 0
   end = toPositiveDim(end, selfType.getShape()[dim]);
   end = std::min(end, selfType.getDimSize(dim));
