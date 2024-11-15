676e482b41aa19c43d6f34ee520ac6ce2415dc2b
Matthias Gehre <matthias.gehre@amd.com>
2024-11-15 18:01:33 +0100

    aten.pow: Fix integer argument accuracy
    Torch uses f64 internally to compute pow if one argument is integer.

--- lib/Conversion/TorchOnnxToTorch/DefaultDomainGtoP.cpp
+++ lib/Conversion/TorchOnnxToTorch/DefaultDomainGtoP.cpp
@@ -2957,35 +2957,14 @@ void mlir::torch::onnx_c::populateDefaultDomainGtoP(
         }
 
         auto loc = binder.getLoc();
-        auto lhsTy = cast<Torch::ValueTensorType>(lhs.getType());
-        auto rhsTy = cast<Torch::ValueTensorType>(rhs.getType());
         Value cstFalse = rewriter.create<Torch::ConstantBoolOp>(
             loc, rewriter.getBoolAttr(false));
         Value none = rewriter.create<Torch::ConstantNoneOp>(loc);
-        auto torchDtype = Torch::getScalarTypeForType(rewriter.getF32Type());
-        Value tyConst = rewriter.create<Torch::ConstantIntOp>(
-            binder.getLoc(), rewriter.getType<Torch::IntType>(),
-            rewriter.getIntegerAttr(rewriter.getIntegerType(64),
-                                    static_cast<int64_t>(torchDtype)));
-
-        if (isa<IntegerType>(lhsTy.getDtype())) {
-          lhsTy = rewriter.getType<Torch::ValueTensorType>(
-              lhsTy.getSizes(), rewriter.getF32Type());
-          lhs = rewriter.create<Torch::AtenToDtypeOp>(loc, lhsTy, lhs, tyConst,
-                                                      cstFalse, cstFalse, none);
-        }
-
-        if (isa<IntegerType>(rhsTy.getDtype())) {
-          rhsTy = rewriter.getType<Torch::ValueTensorType>(
-              rhsTy.getSizes(), rewriter.getF32Type());
-          rhs = rewriter.create<Torch::AtenToDtypeOp>(loc, rhsTy, rhs, tyConst,
-                                                      cstFalse, cstFalse, none);
-        }
 
         auto powType = resultType;
         if (isa<IntegerType>(resultType.getDtype())) {
           powType = rewriter.getType<Torch::ValueTensorType>(
-              resultType.getSizes(), rewriter.getF32Type());
+              resultType.getSizes(), rewriter.getF64Type());
         }
 
         Value pow = rewriter.create<Torch::AtenPowTensorTensorOp>(loc, powType,
