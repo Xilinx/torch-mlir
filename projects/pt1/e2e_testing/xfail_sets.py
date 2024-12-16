266c06a5351a7ba9dcb358963d7dcfef095640df
Matthias Gehre <matthias.gehre@amd.com>
2024-12-16 16:35:01 +0100

    Update xfail

--- projects/pt1/e2e_testing/xfail_sets.py
+++ projects/pt1/e2e_testing/xfail_sets.py
@@ -34,6 +34,11 @@ LINALG_XFAIL_SET = COMMON_TORCH_MLIR_LOWERING_XFAILS | {
     "UnfoldModule_basic",
     # missing lowering from aten.pow.Tensor_Tensor for integer result
     "PowIntIntModule_basic",
+    # unimplemented: only support cases where input and output size are equal for non-unit output size
+    "AdaptiveMaxPool1dDimOneStatic_basic",
+    "AdaptiveMaxPool1dDynamicNoBatch_basic",
+    "AdaptiveMaxPool1dDynamic_basic",
+    "AdaptiveMaxPool1dStatic_basic",
 }
 
 if torch_version_for_comparison() < version.parse("2.5.0.dev"):
