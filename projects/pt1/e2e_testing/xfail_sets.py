2bac738d98e1658dab17653195f9c59ed2be6358
Matthias Gehre <matthias.gehre@amd.com>
2024-12-16 23:21:44 +0100

    update xfail

--- projects/pt1/e2e_testing/xfail_sets.py
+++ projects/pt1/e2e_testing/xfail_sets.py
@@ -1877,7 +1877,6 @@ TOSA_PASS_SET = {
     "CloneModule_basic",
     "ChunkListUnpackUneven_Module_basic",
     "ChunkListUnpack_Module_basic",
-    "ChunkListUnpackUneven_Module_basic",
     "ConstantBoolParameterModule_basic",
     "ConstantPad2dStaticModule_basic",
     "ConstantPadNdModule_basic",
@@ -2533,7 +2532,8 @@ if torch_version_for_comparison() < version.parse("2.5.0.dev"):
 if torch_version_for_comparison() > version.parse("2.6.0.dev"):
     MAKE_FX_TOSA_PASS_SET = MAKE_FX_TOSA_PASS_SET | {
         "ScaledDotProductAttentionDifferentDynamicCausalModule_basic",
-    } - {
+    }
+    MAKE_FX_TOSA_PASS_SET = MAKE_FX_TOSA_PASS_SET - {
         "ChunkListUnpackUneven_Module_basic",
         "ChunkListUnpack_Module_basic",
         "SplitTensorGetItem_Module_basic",
